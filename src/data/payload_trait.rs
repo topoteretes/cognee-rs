a8d900d02db733f3a472a8f7ac98b7dc025d1482
hajdul88 <52442977+hajdul88@users.noreply.github.com>
2025-09-10 16:18:04 +0200

    forgot to add the trait..

---
+++ src/data/payload_trait.rs
@@ -0,0 +1,15 @@
+use crate::data::payload_types::cognee_payload::PropertyStatus;
+use uuid::Uuid;
+
+pub trait PayloadTrait: Send + Sync + 'static {
+    fn payload_id(&self) -> Uuid;
+    fn payload_get_property_status(&self, property: &str) -> Option<PropertyStatus>;
+    fn payload_set_property_status(&self, property: &str, status: PropertyStatus);
+    fn payload_get_arc(&self, property: &str) -> Result<Box<dyn std::any::Any + Send + Sync>, String>;
+    fn payload_get_copy(&self, property: &str) -> Result<Box<dyn std::any::Any + Send + Sync>, String>;
+}
+
+
+pub trait PayloadConstructor: PayloadTrait {
+    fn new(chunks: Vec<std::sync::Arc<String>>) -> Self;
+}
